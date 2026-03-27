aa2d6eeda14e7e72492cb4606515c81910caf16d
Shreyash Raut <shreyashraut9003@gmail.com>
2026-03-27 12:05:15 +0530

    Create Arithmatic_operationswitch.c

---
+++ Arithmatic_operationswitch.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+  void main()
+  {
+    int a,b,c,m,d,s,choice;
+printf("enter two numbers:");
+scanf("%d%d\n",&a,&b);
+printf("Enter your choice:");
+scanf("%d",&choice);
+switch(choice)
+{
+case 1:
+    c=a+b;
+    printf("Addition is %d\n",c);
+    break;
+case 2:
+    s=a-b;
+    printf("subtraction is :%d",s);
+    break;
+case 3:
+    m=a*b;
+    printf("Multiplication is :%d",m);
+    break;
+case 4:
+    d=a/b;
+    printf("Division is :%d",d);
+
+default:
+printf("invalid input");
+    break;
+}
+
+
+
+
+  }
